dc2e6b4d1d63da366e760e1b112c1f2503ed4b4d
mityaginvs <mityaginvs@yandex.ru>
2022-12-30 10:04:00 +0300

    добавлен инпут с калькулятором ширины

--- src/components/Calc/components/Input.js
+++ src/components/Calc/components/Input.js
@@ -1,11 +1,123 @@
 import React from 'react';
+import button__plus from '../../../images/button_plus.png'; 
+import button__minus from '../../../images/button_minus.png'; 
 
 function Input() {
+
+  const [valueInput, setValueInput] = React.useState({
+    userValue: "",
+  });
+  const [ focused, setFocused ] = React.useState(false);
+  const [ disabledLabel, setDisabledLabel ] = React.useState({
+    textLabel: "",
+    status: false,
+  });
+  const [disabledPlus, setDisabledPlus] = React.useState(false);
+  const [disabledMinus, setDisabledMinus] = React.useState(false);
+
+  function handleFocus(e) {
+    setFocused(true);
+  }
+
+  function hideLabel() {
+    setDisabledLabel({...disabledLabel, status: false, textLabel: ''});
+  }
+
+  function showErrorLabel(text) {
+    setDisabledLabel({...disabledLabel, status: true, textLabel: text});
+  }
+
+  function onChange(e) {
+    if (e.target.value === 50) {
+      setDisabledMinus(true); 
+    } else if (e.target.value === 300) {
+      setDisabledPlus(true);
+    }
+
+    setValueInput({...valueInput, [e.target.name]: e.target.value });
+    if (e.target.value > 50 || e.target.value < 300) {
+      hideLabel();
+      setDisabledMinus(false);
+      setDisabledPlus(false);
+    }
+
+    if(Number(e.target.value) < 0) {
+      setValueInput({...valueInput, userValue: ''});
+      showErrorLabel('Введите число от 50 до 300');
+    } else if(Number(e.target.value) < 50) {
+      showErrorLabel('Длина не может быть меньше 50');
+      setDisabledMinus(true);
+    } else if(Number(e.target.value) > 300) {
+      setValueInput({...valueInput, userValue: 300});
+      showErrorLabel('Длина не может быть больше 300');
+      setDisabledPlus(true);
+    }
+  }
+  
+  function onClickPlus(e) {
+    if(valueInput.userValue === '' || Number(valueInput.userValue) < 50) {
+      setValueInput({...valueInput, userValue: 50});
+      setDisabledMinus(true);
+      setDisabledPlus(false);
+    } else if(Number(valueInput.userValue) < 300) {
+      const thisNumber = Number(valueInput.userValue) + 1;
+      setValueInput({...valueInput, userValue: thisNumber});
+      hideLabel();
+      setDisabledMinus(false);
+    } else {
+      showErrorLabel('Длина не может быть больше 300');
+      setDisabledPlus(true);
+    }
+  }
+
+  function onClickMinus(e) {
+    if(valueInput.userValue === '') {
+      setValueInput({...valueInput, userValue: 50});
+      setDisabledMinus(true);
+      setDisabledPlus(false);
+    } else if(Number(valueInput.userValue) > 50) {
+      const thisNumber = Number(valueInput.userValue) - 1;
+      setValueInput({...valueInput, userValue: thisNumber});
+      hideLabel();
+    } else {
+      showErrorLabel('Длина не может быть меньше 50');
+      setDisabledMinus(true);
+    }
+  }
   
   return (
-    <div className="number">
-      <input className="number__input" type="number" max={300} min={50} step={1} placeholder="Введите длину в см" ></input>
-    </div>
+    <>
+      <div className="number">
+        <img 
+          className={`number__button ${disabledMinus ? 'number__button_disabled' : ''}`}
+          alt="кнопка минус" 
+          src={button__minus}  
+          onClick={onClickMinus} 
+        />
+        <input 
+          className="number__input" 
+          type="number" 
+          max={300} 
+          min={50} 
+          step={1} 
+          placeholder="Введите длину в см от 50 до 300"
+          focused={focused.toString()}
+          onBlur={handleFocus}
+          onChange={onChange} 
+          name="userValue"
+          label="Ширина"
+          value={valueInput.userValue}
+        >
+        </input>
+        <img 
+          className={`number__button ${disabledPlus ? 'number__button_disabled' : ''}`} 
+          alt="кнопка плюс" 
+          src={button__plus} 
+          onClick={onClickPlus} 
+        />
+      </div>
+      <label className={`${disabledLabel.status ? 'number__label' : ''}`}>{disabledLabel.textLabel}</label>
+    </>
   );
 }
 
